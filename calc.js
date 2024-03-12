17286a07a100e69cf72abf5337629873ab1142aa
fubiknees <85858509+fubiknees@users.noreply.github.com>
2024-03-11 18:46:50 -0700

    backspace functionality done. Next - change sign function button.

--- calc.js
+++ calc.js
@@ -2,6 +2,11 @@ let operator = '';
 let previousValue = '';
 let currentValue = '';
 
+//previousValue is the first number plus operator
+//currentValue is the second number 
+
+
+//connecting all relevant html divs to javascript via variables
 document.addEventListener("DOMContentLoaded", function(){
     let operators = document.querySelectorAll(".operator");
     let numbers = document.querySelectorAll(".number");
@@ -10,56 +15,81 @@ document.addEventListener("DOMContentLoaded", function(){
     let backspace = document.querySelector(".btn-backspace");
     let clear = document.querySelector(".btn-clear");
     let equal = document.querySelector(".btn-equal");
-    let decimal = document.querySelector("btn-decimal");
+    let decimal = document.querySelector(".btn-decimal");
 
     let previousScreen = document.querySelector(".previous");
     let currentScreen = document.querySelector(".current"); 
 
+    //function that goes through all "number" divs(name of variable we assigned), AEL on click
+    //which allows for it to extract textContent value of all number buttons 
     numbers.forEach((number) =>  number.addEventListener("click", function(event){
         outputNum(event.target.textContent)
-        currentScreen.textContent = currentValue;
+        currentScreen.textContent = currentValue;//Allows for input 
         //utilizes text property so the number on the button outputs when clicked
     }));
 
+
+    //operator function that clears currentScreen and displays both number and operator on previousScreen
     operators.forEach((op) => op.addEventListener("click", function(event){
         outputOp(event.target.textContent)
-        previousScreen.textContent = previousValue + " " + operator;
-        currentScreen.textContent = currentValue;
+        previousScreen.textContent = previousValue + " " + operator; //creates format for previousScreen
+        currentScreen.textContent = currentValue; //allows for input separate from previous input
     }))
 
+    //clears everything
     clear.addEventListener("click", function(event){
         operator = '';
-        previousValue = '';
+        previousValue = '';//clears values
         currentValue = '';
-        previousScreen.textContent = currentValue;
+        previousScreen.textContent = currentValue; //clears screens
         currentScreen.textContent = currentValue;
     })
 
+    //function that creates total in the currentScreen and clears previousScreen
     equal.addEventListener("click", function(){
-        calculate();
+        if(currentValue != '' && previousValue != ''){
+        operate();
+        previousScreen.textContent = ''; //clears top screen 
+            if (previousValue.length <= 9){
+                currentScreen.textContent = previousValue;   
+            } else {
+                currentScreen.textContent = previousValue.slice(0,9) + "...";
+            }
+        }   
     })
+
+    decimal.addEventListener("click", function(){
+        addDecimal();
+    })
+
+    backspace.addEventListener("click", function(){
+        currentScreen.textContent = currentScreen.textContent.toString().slice(0,-1);
+        currentValue = currentScreen.textContent
+        console.log("backspace");
+    })  
  })
 
- //outputs number entered
+ //allows adding of individual digits < 8 in length
  function outputNum(num){
-    if (currentValue.length <= 6){
-        currentValue += num;
+    if (currentValue.length <= 8){ //limits length using if statement
+        currentValue += num; //adds individual digits
     }
  }
 
+//moves info to previousValue and empties currentValue on trigger
  function outputOp(op){
-    operator = op;
-    previousValue = currentValue;
-    currentValue = '';
+    operator = op; //updates global variable
+    previousValue = currentValue; //moves currentValue to previousValue 
+    currentValue = ''; //and empties currentValue to allow for input
  }
 
- function calculate() {
-    //convert string into Numbers for arithmetic
+ function operate() {
+    //convert string input into Numbers for arithmetic
     previousValue = Number(previousValue);
     currentValue = Number(currentValue);
 
     if (operator  === "+"){
-        previousVaue += currentValue;
+        previousValue += currentValue;
     }
     else if (operator === "-"){
         previousValue -= currentValue;
@@ -72,5 +102,20 @@ document.addEventListener("DOMContentLoaded", function(){
         previousValue /= currentValue;
     }
 
-    console.log(previousValue)
+
+    ///after arithmetic is done convert back to string for display output
+    previousValue = roundNumber(previousValue);
+    previousValue = previousValue.toString();
+    currentValue = previousValue.toString();
  } 
+
+ //rounds number 
+ function roundNumber (num){
+    return Math.round(num *10000)/10000; 
+ }
+
+ function addDecimal(){
+    if(!currentValue.includes(".")){
+        currentValue += '.';
+    }
+ }
